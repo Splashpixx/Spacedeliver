9e163f1daaa7a3b5ba5f5c6123e711e9a8cb61d5
Domenic Wolf <53898442+Splashpixx@users.noreply.github.com>
2020-01-11 17:16:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+Primäre Datei für die APIs
+*/
+
+
+// Dipendencies
+var server = require('./lib/server');
+
+
+// declare the app
+var app = {};
+
+
+app.init = function () {
+	//start server
+	server.init();
+}
+
+// execute
+app.init();
+
+// export app
+module.exports = app;
+
+
+
+/*
+Keystore password: keystorepassword
+Key alias: keyalias
+*/
+
