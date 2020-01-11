b5b882435ff74548e2365228d9d6a325c631a305
Domenic Wolf <53898442+Splashpixx@users.noreply.github.com>
2020-01-11 18:26:21 +0100

    Add files via upload

--- Dienstgeber/lib/handlers/cart-Handler.js
+++ Dienstgeber/lib/handlers/cart-Handler.js
@@ -20,39 +20,39 @@ _cart.post = function (data, callback) {
 
 		_tokens.verifyToken(tokens, UserName, function (tokenIsValid, tokenData) {
 					
-		if (!tokenIsValid) 
-		{
-			callback(403, {'error':'token is invalid'});
-		} 
-		else 
-		{
-			
-			// look if the items are here
-			var allRight = helpers.lookIfeverythingIsallRight(data.payload);
+			if (!tokenIsValid) {
+				callback(403, {'error':'token is invalid'});
+			} else {
+				
+				// look if the items are here
+				var allRight = helpers.lookIfeverythingIsallRight(data.payload);
 
-				if (!allRight) {
-					callback(403, {'Error' : 'invalid order' });
-				}else {
-					
-					
-					// ab hier funktioniert alles
-			// create the order
-					var orderId = Date.now();
-					var orderFileName = helpers.orderName(orderId, UserName);
-					var orderData =  {'orderId': orderId, 'items': allRight };
+					if (!allRight) {
+//						var texscht = fs.readFileSync(helpers.baseDir+'/'+'my'+'.json', 'utf8');
+//						console.log(texscht);
+						callback(403, {'Error' : 'invalid order' });
+						console.log(data);
+					}else {
 						
-					//Create a JSON file for the order
-					_data.create('orders', orderFileName, orderData, function(err){
-						if (err){
-							callback(500, {'Error' : 'Error adding item to the order.'});				
-						}
-						else{
-							console.log(`Order ${orderId} was saved to file: ` + JSON.stringify(orderData));
-							callback(200, {'Message' : 'The order was received', 'orderId' : orderId });
-						}
-					});
-				}
-		}
+						
+						// ab hier funktioniert alles
+				// create the order
+						var orderId = Date.now();
+						var orderFileName = helpers.orderName(orderId, UserName);
+						var orderData =  {'orderId': orderId, 'items': allRight };
+							
+						//Create a JSON file for the order
+						_data.create('orders', orderFileName, orderData, function(err){
+							if (err){
+								callback(500, {'Error' : 'Error adding item to the order.'});				
+							}
+							else{
+								console.log(`Order ${orderId} was saved to file: ` + JSON.stringify(orderData));
+								callback(200, {'Message' : 'The order was received', 'orderId' : orderId });
+							}
+						});
+					}
+			}
 	});		
 };
 
